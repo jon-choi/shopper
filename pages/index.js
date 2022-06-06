dc670e6872a94a1237ebcb9c3824dd187d67d599
Jonathan Choi <jonchoi11@gmail.com>
2022-06-06 11:01:28 -0600

    connecting sanity to project

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 
+import { client } from "../lib/client";
 import { Product, FooterBanner, HeroBanner } from "../components";
 
-const Home = () => {
+const Home = ({ products, bannerData }) => {
   return (
     <>
       <HeroBanner />
@@ -11,11 +12,23 @@ const Home = () => {
         <p>Many, many speakers</p>
       </div>
       <div className="products-container">
-        {["Product 1", "Product 2"].map((product) => product)}
+        {products?.map((product) => product.name)}
       </div>
       <FooterBanner />
     </>
   );
+};
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData },
+  };
 };
 
 export default Home;
