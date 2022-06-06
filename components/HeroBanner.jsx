7aba9dc034c6bd11a56cef58086d75e129cbe1ae
Jonathan Choi <jonchoi11@gmail.com>
2022-06-06 10:46:31 -0600

    start on hero banner component

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
 
 const HeroBanner = () => {
   return (
-    <div>HeroBanner</div>
+    <div className="hero-banner-container">
+      <div>
+        <p className="beats-solo">small text</p>
+        <h3>mid text</h3>
+        <img src="" alt="headphones"
+        className="hero-banner-image" />
+
+        <div>
+          <Link href="/product/ID">
+            <button type="button">button text</button>
+          </Link>
+          <div className="desc">
+            <h5>description</h5>
+            <p>DESCRIPTION</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
