f337839b0e139dcc69f398dc5e1987c7ae1bc39a
Jonathan Choi <jonchoi11@gmail.com>
2022-06-08 12:08:25 -0600

    add images container to map over all items

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -12,6 +12,15 @@ const ProductDetails = ({ product, products }) => {
           <div className="image-container">
             <img src={urlFor(image && image[0])} />
           </div>
+          {/* <div className="small-images-container">
+            {image?.map((item, i) => (
+              <img 
+                src={urlFor(item)}
+                className=""
+                onMouseEnter=""
+              />
+            ))}
+          </div> */}
         </div>
       </div>
     </div>
