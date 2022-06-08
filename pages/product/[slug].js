8b1392e25e6e7045fec141fd81f98bf8046baa75
Jonathan Choi <jonchoi11@gmail.com>
2022-06-08 11:54:39 -0600

    implement static props function

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,7 +1,31 @@
 import React from "react";
 
+import { client, urlFor } from "../../lib/client";
+
 const ProductDetails = () => {
-  return <div>Product</div>;
+  return (
+    <div>
+      <div className="product-detail-container">
+        <div>
+          <div className="image-container">
+            <img src="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const getStaticProps = async ({ params: { slug } }) => {
+  const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
+  const productsQuery = '*[_type == "product"]';
+
+  const product = await client.fetch(query);
+  const products = await client.fetch(productsQuery);
+
+  return {
+    props: { product, products },
+  };
 };
 
 export default ProductDetails;
