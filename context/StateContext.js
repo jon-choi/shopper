56a3a83a64b118de5f8e876e842e8371fe51315e
Jonathan Choi <jonchoi11@gmail.com>
2022-06-14 11:07:49 -0600

    add add to cart functionality with some comments for clarity

--- context/StateContext.js
+++ context/StateContext.js
@@ -10,6 +10,38 @@ export const StateContext = ({ children }) => {
   const [totalQuantities, setTotalQuantities] = useState();
   const [qty, setQty] = useState(1);
 
+  // function to add items to cart
+  // also checks if product is already in cart
+  // if item is already in the cart, only increase quantity of item and price
+  const onAdd = (product, quantity) => {
+    const checkProductInCart = cartItems.find(
+      (item) => item._id === product._id
+    );
+
+    if (checkProductInCart) {
+      setTotalPrice(
+        (prevTotalPrice) => prevTotalPrice + product.price * quantity
+      );
+      setTotalQuantities(
+        (prevTotalQuantities) => prevTotalQuantities + quantity
+      );
+
+      // map over current cart items
+      // get each individual item
+      // increase quantity of product
+      const updatedCartItems = cartItems.map((cartProduct) => {
+        if (cartProduct._id === product._id)
+          return {
+            ...cartProduct,
+            quantity: cartProduct.quantity + quantity,
+          };
+      });
+
+      setCartItems(updatedCartItems);
+      toast.success(`${qty} ${product.name} added to cart.`);
+    }
+  };
+
   // increases qty of items
   const incQty = () => {
     setQty((prevQty) => prevQty + 1);
